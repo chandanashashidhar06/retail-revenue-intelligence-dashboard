8d2d6dd58ff071d5ee3969ba5821d0c2046ac226
chandanashashidhar06 <chandanashashidhar6@gmail.com>
2026-01-17 13:13:31 +0530

    Update revenue_analysis.sql

--- revenue_analysis.sql
+++ revenue_analysis.sql
@@ -1,1 +1,20 @@
--- SQL queries for overall revenue analysis
+-- 1. Total Revenue
+SELECT SUM("Total_Cost") AS Total_Revenue
+FROM transactions;
+
+-- 2. Discount vs Non-Discount Revenue
+SELECT "Discount_Applied", SUM("Total_Cost") AS Revenue
+FROM transactions
+GROUP BY "Discount_Applied";
+
+-- 3. Discount Dependency %
+SELECT 
+    (SUM(CASE WHEN "Discount_Applied" = 'true' THEN "Total_Cost" ELSE 0 END)
+     / SUM("Total_Cost")) * 100 AS Discount_Dependency_Percentage
+FROM transactions;
+
+-- 4. Average Order Value (AOV)
+SELECT 
+    SUM("Total_Cost") / COUNT("Transaction_ID") AS AOV
+FROM transactions;
+
